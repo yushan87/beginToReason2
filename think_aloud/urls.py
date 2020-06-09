303190fd103a05ffd419965f2651a1ba2590d5ea
Yu-Shan Sun <yushans@g.clemson.edu>
2020-06-09 02:50:13 -0400

    Minor fixes

---
+++ think_aloud/urls.py
@@ -0,0 +1,13 @@
+"""
+This module contains our mapping of URL path expressions to Django views for the "tutor" application.
+"""
+from django.urls import path
+
+from . import views
+
+# URL namespace for this application.
+app_name = 'think_aloud'
+
+# URL patterns to be matched.
+urlpatterns = [
+]
