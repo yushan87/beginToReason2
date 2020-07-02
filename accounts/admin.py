9d172403aaab7e900de2c49cc5a120b9e42c7f89
Yu-Shan Sun <yushans@g.clemson.edu>
2020-07-01 23:07:00 -0400

    Changed the UserInformation model to include a user (foreign key) and renamed user_name to user_nickname

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,9 +3,26 @@ This module registers the models we created for the "accounts" application. Afte
 the model, the data will be accessible through Django's admin functionality.
 """
 from django.contrib import admin
-
 from .models import UserInformation
 
 
+class UserInformationAdmin(admin.ModelAdmin):
+    """
+    This class contains rendering details for the UserInformation table
+    """
+    model = UserInformation
+    list_display = ('get_user_email', 'user_nickname', 'user_school', 'user_class')     # Fields to be shown initially
+
+    def get_user_email(self, obj):
+        """function get_user_email is an helper function to retrieve the email associated with the user
+
+            Returns:
+                str: user email
+        """
+        return obj.user.email
+
+    get_user_email.short_description = 'User Email'  # Renames column head
+
+
 # Register your models here.
-admin.site.register(UserInformation)
+admin.site.register(UserInformation, UserInformationAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -3,29 +3,27 @@ This module contains model templates for the "accounts" application. In particul
 database tables related to the user account information. When we create a new item in the database,
 a new instance of a model will be made.
 """
-# from datetime import datetime
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 from django.db import models
-
-# Create your models here.
 
 
 class UserInformation(models.Model):
     """
-    Contains a model of a user to keep track of user information
-    @param models.Model
+    Contains a model of a user to keep track of user information.
     """
-    # TODO: documentation needs work.
-    user_email = models.EmailField()
-    user_name = models.TextField()
-    user_school = models.CharField(max_length=50)
-    user_class = models.CharField(max_length=50)
-    user_gender = models.CharField(max_length=50)
-    user_race = models.CharField(max_length=50)
+    # All of the fields in the model has validators to make sure they are valid.
+    user = models.ForeignKey(User, on_delete=models.CASCADE)    # Foreign key referring to an entry in the user table
+    user_nickname = models.CharField("Nickname", max_length=25, validators=[MinLengthValidator(1)])  # Nickname field
+    user_school = models.CharField("School", max_length=50, validators=[MinLengthValidator(1)])      # School field
+    user_class = models.CharField("Class", max_length=50, validators=[MinLengthValidator(1)])        # Class name field
+    user_gender = models.CharField("Gender", max_length=50, validators=[MinLengthValidator(1)])      # Gender field
+    user_race = models.CharField("Race", max_length=50, validators=[MinLengthValidator(1)])          # Race field
 
     def __str__(self):
-        """function __str__ is called on a user to retrieve information
+        """function __str__ is used to create a string representation of this class
 
             Returns:
-                str: user name
+                str: user email
         """
-        return self.user_email
+        return self.user.email
