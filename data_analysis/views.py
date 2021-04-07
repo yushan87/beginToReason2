1e0b46dd4811d90d643d0f2b1be372b55c84828e
rhit-hurtigna <73034845+rhit-hurtigna@users.noreply.github.com>
2021-04-07 16:53:26 -0400

    Adding documentation for views

--- data_analysis/views.py
+++ data_analysis/views.py
@@ -8,13 +8,29 @@ from data_analysis.py_helper_functions.graph_viewer.lesson_reader import lesson_
 
 
 # Create your views here.
-def d3_graph(request, set_id, lesson_index):
+def d3_graph(request, setID, lessonIndex):
+    """function d3_graph This function handles the view for the graph of a specific lesson
+
+    Args:
+        request (HTTPRequest): A http request object created automatically by Django.
+
+    Returns:
+        HttpResponse: A generated http response object to the request to generate the graph
+    """
     print(request)
-    return render(request, "data_analysis/d3graph.html", {'graphData': lesson_to_json(set_id, lesson_index, True),
-                                                          'lessonData': lesson_info(set_id, lesson_index)})
+    return render(request, "data_analysis/d3graph.html", {'graphData': lesson_to_json(setID, lessonIndex, True),
+                                                          'lessonData': lesson_info(setID, lessonIndex)})
 
 
 def set_statistics(request, index):
+    """function d3_graph This function handles the view for lesson set-wide stats
+
+    Args:
+        request (HTTPRequest): A http request object created automatically by Django.
+
+    Returns:
+        HttpResponse: A generated http response object to the request to show the lesson set
+    """
     print(request)
     return render(request, "data_analysis/setStatistics.html", {'lessonSetData': get_set_stats(index),
                                                                 'lessonSetInfo': get_set_info(index)})
