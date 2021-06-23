b020cfd608a73075a57c1be7c2c34353cf7857f0
Yu-Shan Sun <yushans@g.clemson.edu>
2021-06-23 14:21:53 -0400

    Fixed privacy page and separated back-to-top into a separate component

---
+++ core/static/javascript/components/backToTop.js
@@ -0,0 +1,28 @@
+/*
+ * This contains the JavaScript code required for a button that allows us to go back to the top.
+ * Source: https://mdbootstrap.com/docs/standard/extended/back-to-top/
+*/
+
+//Get the button
+let myButton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+myButton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
