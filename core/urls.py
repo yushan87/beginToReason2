69fe8fa9f7942106e976bde26b0eaeaec9181805
Yu-Shan Sun <yushans@g.clemson.edu>
2021-09-14 17:46:11 -0400

    Deleted unused images, begin reorganizing images in use and fixed issue with favicon

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 """
 This module contains our mapping of URL path expressions to Django views for the "core" application.
 """
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
@@ -10,5 +12,6 @@ app_name = 'core'
 
 # URL patterns to be matched.
 urlpatterns = [
-    path('privacy', views.privacy, name='privacy')
+    path('privacy', views.privacy, name='privacy'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico')))
 ]
