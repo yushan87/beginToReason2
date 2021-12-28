989b4b858c99a380844fcf908dac42880109c722
Yu-Shan Sun <yushans@g.clemson.edu>
2021-12-27 23:07:42 -0500

    Created a template for tutor and parson lessons + finish migrating the tutor ones

--- tutor/static/javascript/oldJS/injectLeft.js
+++
@@ -1,8 +0,0 @@
-/* global loadLessonFromUrl */
-
-function injectTeach(id, module) {
-    $("#" + id).load("/html/left.html", function () {
-        var url = "/section/" + module + "/initial";
-        loadLessonFromUrl(url);
-    });
-}
